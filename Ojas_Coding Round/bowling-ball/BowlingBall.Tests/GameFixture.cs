8a3b47c23f42ffe4b48e452d6359b60b3001e3ac
LTOjas <62498650+LTOjas@users.noreply.github.com>
2020-03-22 14:38:31 +0530

    Add files via upload
    Complete Folder Upload

---
+++ Ojas_Coding Round/bowling-ball/BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            var game = new Game();
+            Roll(game, 0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_test()
+        {
+            var game = new Game();
+            game.Roll(10);
+
+            game.Roll(9);
+            game.Roll(1);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            game.Roll(7);
+            game.Roll(2);
+
+            game.Roll(10);
+
+            game.Roll(10);
+
+            game.Roll(10);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(8);
+            game.Roll(2);
+
+            game.Roll(9);
+            game.Roll(1);
+
+            game.Roll(10);
+
+            Assert.AreEqual(187, game.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ Ojas_Coding Round/bowling-ball/BowlingBall/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        public int score = 0;
+        public int bonus = 0;
+        List<int> pinsVal = new List<int>();
+
+        public void Roll(int pins)
+        {
+            pinsVal.Add(pins);
+            score += pins;
+        }
+
+        public int GetScore()
+        {
+            // Returns the final score of the game.
+            calulateBonus();
+            return score + bonus;
+        }
+
+        public void calulateBonus()
+        {
+            for (int i = 0; i < pinsVal.Count(); i++)
+            {
+                if (i <= (pinsVal.Count() - 2))
+                {
+                    if (pinsVal[i] == 10)
+                    {
+                        bonus += pinsVal[i + 1];
+                        bonus += pinsVal[i + 2];
+                    }
+
+                    if(((pinsVal[i] + pinsVal[i+1]) == 10) && (pinsVal[i-1] != 10))
+                    {
+                        bonus += pinsVal[i + 2];
+                    }
+                }
+            }
+        }
+    }
+}
